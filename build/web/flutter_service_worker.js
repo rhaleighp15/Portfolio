f2afc324c6f2f276df23b0152821ffe77995c842
Marianne Rhaleigh G. Paradero <marianne.paradero@lpunetwork.edu.ph>
2025-12-04 14:22:59 +0800

    Use PORT-based nginx template for Railway

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -134,7 +134,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "8c9aecba21ee2dc4211afa246c06390a
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "ac4087db0d8cb5b92518a458a58ed953",
+"flutter_bootstrap.js": "b64c8e8e0cced90afd1431a8e2df0587",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
